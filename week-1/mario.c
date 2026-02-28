1dded02d02132836f7a3e77376b094a0add95fc4
Anika Feta <anika.feta@ibu.edu.mk>
2026-02-28 23:38:03 +0100

    Fix variable declaration for height input
    Declare variable 'n' before using it in the while loop.

--- week-1/mario.c
+++ week-1/mario.c
@@ -3,14 +3,13 @@
 
 int main(void)
 {
-    int n = get_int("Height: ");
+    int n;
     while(n <= 0 || n > 8) {
         n = get_int("Height: ");
     }
 
     for(int i = 0; i < n; i++) {
         for(int w = n; w > i + 1; w--) {
-
             printf(" ");
         }
         for(int w = 0; w <= i; w++) {
